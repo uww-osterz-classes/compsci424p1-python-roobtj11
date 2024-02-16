a34853ead14c042b9693abeb10f24004bafeccd7
roobtj11 <79338430+roobtj11@users.noreply.github.com>
2024-02-16 11:39:04 -0600

    working

--- p1main copy.py
+++ p1main copy.py
@@ -1,124 +1,255 @@
-
 """
 COMPSCI 424 Program 1
 Name: Tyler Roob
 """
 import time
 
-
-
-PCBs = []
-
-class PCB:
-  def __init__(self,pid):
-    self.pid = pid
-    self.parent = -1
-    self.children = [None]
-  def __init__(self,parent, children):
-    self.parent = parent
-    self.children = [children]
-
+#Initialize Process Classes
+class V1_Process:
   def __init__(self,parent):
     self.parent = parent
-    self.children = [None]
+    self.children =[]
 
-  def __set_children__(self,children):
+  def add_children(self,children):
       self.children.append(children)
-      if(self.children[0]==None):
-        self.children.pop(0)
-      
-def showProcessInfo():
-  for x in range(0,len(PCBs)):
-    if PCBs[x].children[0]!=None:
+
+  def remove_child(self,child_pid):
+    if child_pid in self.children:
+      self.children.remove(child_pid)
+class V2_Process:
+  def __init__(self,Parent,osib=None):
+    self.Parent = Parent
+    self.First_Child = None
+    self.Younger_Sibiling = None
+    self.Older_Sibiling = osib
+
+  def set_y_sib(self):
+    if([self.parent].fchild):
+      None
+
+#***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***#
+def v1(Commands,show_output):
+  PCB = create_V1_PCB()
+  for command in Commands:
+    Action=command[0]
+    match Action:
+      case 'c':
+        parent = command[1]
+        process = First_Available_Process(PCB)
+        #print(f"Creating Process {process} with parent {parent}")
+        v1_create(PCB,process,parent)
+      case 'd':
+        process = command[1]
+        #print(f"Deleting Process {process}, and all children")
+        v1_destroy(PCB,process)
+    if show_output:
+      v1_showProcessInfo(PCB)
+
+def v1_create(PCB,process,parent):
+  try:
+    #Link to Parent
+    PCB[parent].add_children(process)
+    #Create Process
+    PCB[process] = V1_Process(parent)
+  except:
+    print(f"Error, Parent doesn't exist")
+
+def v1_destroy(PCB,process):
+  parent = PCB[process].parent
+  PCB[parent].remove_child(process)
+  while PCB[process].children != []:
+    for child_index  in PCB[process].children:
+      v1_destroy(PCB,child_index)
+  #print(f"Destroying: {process}")
+  del PCB[process]  
+
+def v1_showProcessInfo(PCBs):
+  for x in PCBs:
+    if PCBs[x].children!=[]:
       children_list = ""
       for c in PCBs[x].children:
         children_list= children_list + " "
         c=str(c)
         children_list= children_list + c
-      print("Process ",x,": parent is ",PCBs[x].parent," and children are",children_list)
+      print(f"Process {x}: parent is {PCBs[x].parent} and children are {children_list}")
     else:
-      print("Process ",x,": parent is ",PCBs[x].parent," and has no children")
+      print(f"Process {x}: parent is {PCBs[x].parent} and has no children")
+  print("")
 
-def first():
-  PCBs.append(PCB(-1))
-  print("Creating New Root")
+#***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***#
+def v2(commands,show_output):
+  PCB = create_V2_PCB()
+  command_num = 0
+  for command in commands:
+    command_num+=1
+    #print(f"\nCommand #{command_num}\n")
+    Action=command[0]
+    if command[1] not in PCB:
+      print(f"Error | Object Does Not exist in PCB")
+    else:
+      match Action:
+        case 'c':
+          parent = command[1]
+          #print(f"\nRunning command 'Create' with Parent {parent}")
+          process = First_Available_Process(PCB)
+          #print(f"Creating Process {process} with parent {parent}")
+          v2_create(PCB,process,parent)
+        case 'd':
+          process = command[1]
+          #print(f"\nRunning command 'Destroy' process {process}")
+          #print(f"Deleting Process {process}, and all children")
+          v2_destroy(PCB,process)
+      if show_output:
+        v2_showProcessInfo(PCB)
+  
+def v2_showProcessInfo(PCB):
+  #print("\nCurrent Processes:")
+  for process in PCB:
+    parent = PCB[process].Parent
+    children = None
+    if PCB[process].First_Child is not None:
+      First_Child = PCB[process].First_Child
+      child = First_Child
+      children = str(First_Child)
+      while True:
+        if PCB[child].Younger_Sibiling is not None:
+          Younger_Sibiling = PCB[child].Younger_Sibiling
+          children+=f" {Younger_Sibiling}"
+          child = Younger_Sibiling
+        else:
+          break
+      print(f"Process {process}: Parent is {parent} Children are {children}")
+    else:
+      print(f"Process {process}: Parent is {parent} and has no children.")
+  print("")
 
-def create(parent):
-  PCBs.append(PCB(parent))
-  PCBs[parent].__set_children__(len(PCBs)-1)
+    
 
-def destroy(p):
-  for x in PCBs[p].children:
-    PCBs.pop(x)
-  PCBs.pop(p)
 
-class commands:
-  def __init__(self, command, n):
-    self.command = command
-    self.n = n
+def v2_destroy(PCB,process):
+  #print(f"Destroy {process}")
+  while True:
+    if PCB[process].First_Child != None: v2_destroy(PCB,PCB[process].First_Child)
+    if PCB[process].First_Child == None: break
+  Parent = PCB[process].Parent
+  if PCB[Parent].First_Child == process:
+    if PCB[process].Younger_Sibiling == None:
+      PCB[Parent].First_Child = None
+    else:
+      PCB[Parent].First_Child = PCB[process].Younger_Sibiling
+  else:
+    Older_Sibiling = PCB[process].Older_Sibiling
+    Younger_Sibiling = PCB[process].Younger_Sibiling
+    if PCB[Younger_Sibiling] == None:
+      PCB[Older_Sibiling].Younger_Sibiling = None
+    else:
+      PCB[Older_Sibiling].Younger_Sibiling = Younger_Sibiling
+      PCB[Younger_Sibiling].Older_Sibiling = Older_Sibiling
+  del PCB[process]
+  #print(f"Process {process} has been deleted")
+
+
+def find_Older_Sibiling(PCB,Parent):
+  Older_Sibiling = PCB[Parent].First_Child
+  while True:
+    if PCB[Older_Sibiling].Younger_Sibiling is not None: Older_Sibiling = PCB[Older_Sibiling].Younger_Sibiling
+    else:
+      return Older_Sibiling
+    
+def v2_create(PCB,process,Parent):
+  try:
+    #Link to Parent
+    #Check First_Child
+    if Parent not in PCB:
+      print(f"ERROR | Parent {Parent} does not exist!")
+      return
+    if PCB[Parent].First_Child is None: #No Sibilings
+      PCB[Parent].First_Child = process
+      PCB[process] = V2_Process(Parent)
+      return
+    else:
+      Older_Sibiling = find_Older_Sibiling(PCB,Parent)
+      PCB[Older_Sibiling].Younger_Sibiling = process
+      PCB[process] = V2_Process(Parent,Older_Sibiling)
+  except:
+    print(f"Error, Parent doesn't exist")
+
+#***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***#
+#                                              #Initialize PCBs and initial Processes
+def create_V1_PCB():
+  PCBs_V1 = {}
+  PCBs_V1[0] = V1_Process(-1)
+  return PCBs_V1
+def create_V2_PCB():
+  PCBs_V2 = {}
+  PCBs_V2[0] = V2_Process(-1)
+  return PCBs_V2
+#                                                     For Both V1 and V2
+def First_Available_Process(PCB):
+  for process_id in range (1,16):
+    if process_id not in PCB:
+      return process_id
+  print("No Available Space")
+  return False
+#                                                     #Initial Functions
+def get_commands():
+  Commands = []
+  while True:
+    New_input = Request_Input()
+    Action = New_input[0]
+    process = New_input[1]
+    if Action == 'e': break
+    if Action == False: 
+      print(f"Error: {process}")
+    else:
+      Commands.append(New_input)
+  return Commands
+def Request_Input():
+  try:
+    new_input = input("Enter commands of the form 'create N', 'destroy N', or 'end', where N is an integer between 0 and 15:")
+    if new_input == "end": return ['e',None]
+    new_input = new_input.split(" ")
+    action = new_input[0]
+    item = int(new_input[1])
+    if action != "create" and action != "destroy" and action != "end": return [False, "Invalid action item"]
+    if item not in range(0,16): return [False, "Invalid Process"]
+    match action:
+      case "create":
+        return ['c',item]
+      case "destroy":
+        if item == 0: return [False,"Invalid Argument"]
+        return ['d',item]
+        
+  except:
+    print("Invalid Argument")
+    return [False,"Invalid Argument"]
 
 def main():
-  commands = list()
-  while True:
-    c = input("enter commands of the form 'create N', 'destroy N', or 'end', where N is an integer between 0 and 15:")
-    c = c.split(" ")
-    if c[0] == 'end':
-      break
-    if len(c) == 2:
-      if c[0] == 'create' or c[0] == 'destroy':
-        if int(c[1]) in range(0,16):
-          commands.append(commands(c[0],int(c[1])))
-        else:
-          ic()
-      else:
-        ic()
-    else:
-      ic()
-  
-
-
-
-def ic():
-  print("Invalid command")
+  Commands=get_commands()
+  print("Beginning V1:\n`")
+  v1(Commands,True)
+  print("\nEnd of V1\n")
+  print("Beginning of V2:\n")
+  v2(Commands,True)
+  print("\nEnd of V2")
+  input("Press Enter to start timings")
+  print("Running V1 200 Times")
+  v1_start = time.process_time_ns()
+  for _ in range(200):
+    v1(Commands,False)
+  v1_end = time.process_time_ns()
+  print("Running V1 200 Times")
+  v2_start = time.process_time_ns()
+  for _ in range(200):
+    v1(Commands,False)
+  v2_end = time.process_time_ns()
+  v1_running = v1_end-v1_start
+  v2_running = v2_end-v1_start
+  print(f"V1 start time: {v1_start}")
+  print(f"V1 end time: {v1_end}")
+  print(f"V1 Running Time: {v1_running}")
+  print(f"V2 start time: {v2_start}")
+  print(f"V2 end time: {v2_end}")
+  print(f"V2 Running Time: {v2_running}")  
 
 main()
-
-
-first(PCBs)
-create(0,PCBs)
-create(1,PCBs)
-create(1,PCBs)
-create(1,PCBs)
-create(3,PCBs)
-showProcessInfo(PCBs)
-"""
-Version 1
-Version 1 of the process creation hierarchy uses linked lists to keep track of child processes. In Version 1, each process's control block (PCB) contains
-
-a pointer to its parent process and
-a linked list of pointers to 0 or more child processes.
-For the purposes of performance evaluation, the PCBs are simplified as follows:
-
-All PCBs are implemented as an array of size n.
-Each process is referred to by the PCB index, 0 through (n - 1).
-Each PCB is a structure consisting of only two fields:
-parent: a PCB index corresponding to the process's creator
-children: a pointer to a linked list, where each list element contains the PCB index of one child process
-The necessary functions are simplified as follows:
-
-create(p) represents the create function executed by process PCB[p]. The function creates a new child process PCB[q] of process PCB[p] by performing the following tasks:
-allocate a free PCB[q]
-record the parent's index, p, in PCB[q]
-initialize the list of children of PCB[q] as empty
-create a new link containing the child's index q and appends the link to the linked list of PCB[p]
-destroy(p) represents the destroy function executed by process PCB[p]. The function recursively destroys all descendant processes (child, grandchild, etc.) of process PCB[p] by performing the following tasks:
-for each element q on the linked list of children of PCB[p]:
-destroy(q) /* recursively destroy all descendants */
-free PCB[q]
-deallocate the element q from the linked list
-"""
-
-# There are many correct ways to solve this in Python, so I'm giving
-# you minimal guidance for your Python code. Any correct solution is
-# allowed.
-
--- p1main.py
+++ p1main.py
@@ -4,117 +4,66 @@ Name: Tyler Roob
 """
 import time
 
-class PCB:
+#Initialize Process Classes
+class V1_Process:
   def __init__(self,parent):
     self.parent = parent
-    self.children = []
+    self.children =[]
 
-  def __set_children__(self,children):
+  def add_children(self,children):
       self.children.append(children)
 
   def remove_child(self,child_pid):
     if child_pid in self.children:
       self.children.remove(child_pid)
+class V2_Process:
+  def __init__(self,Parent,osib=None):
+    self.Parent = Parent
+    self.First_Child = None
+    self.Younger_Sibiling = None
+    self.Older_Sibiling = osib
 
-class PCBv2:
-  def __init__(self,parent,osib=None):
-    self.parent = parent
-    self.fchild = None
-    self.ysib = None
-    self.osib = osib
   def set_y_sib(self):
     if([self.parent].fchild):
       None
 
-def find_OlderSib(PCB,older_sib):
-  if PCB[older_sib].ysib == None:
-    return older_sib
-  else:
-    return find_OlderSib(PCB,PCB[older_sib].ysib)
+#***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***V1***#
+def v1(Commands,show_output):
+  PCB = create_V1_PCB()
+  for command in Commands:
+    Action=command[0]
+    match Action:
+      case 'c':
+        parent = command[1]
+        process = First_Available_Process(PCB)
+        #print(f"Creating Process {process} with parent {parent}")
+        v1_create(PCB,process,parent)
+      case 'd':
+        process = command[1]
+        #print(f"Deleting Process {process}, and all children")
+        v1_destroy(PCB,process)
+    if show_output:
+      v1_showProcessInfo(PCB)
 
-def get_ys(PCBs,pid):
-  if PCBs[pid].ysib != None:
-    a = [True, PCBs[pid].ysib]
-    return a
-  else:
-    return [False]
+def v1_create(PCB,process,parent):
+  try:
+    #Link to Parent
+    PCB[parent].add_children(process)
+    #Create Process
+    PCB[process] = V1_Process(parent)
+  except:
+    print(f"Error, Parent doesn't exist")
 
-def showV2(PCBs):
-  for x in PCBs:
-    children = []
-    t=True
-    if PCBs[x].fchild != None:
-      children.append(PCBs[x].fchild)
-      child = PCBs[x].fchild
-      while (t==True):
-        result = get_ys(PCBs,child)
-        if result[0]:
-          child = result[1]
-          children.append(child)
-        else:
-          t = result[0]
-      print(f"Process {x}: parent is {PCBs[x].parent}, and has children {children}")
-    else:
-      print(f"Process {x}: parent is {PCBs[x].parent}, and has no children")
-def destroy_v2(PCBs,pid):
-  t=1
-  while t==1:
-    if PCBs[pid].fchild is None:
-      parent = PCBs[pid].parent
-      if PCBs[parent].fchild == pid:
-        if PCBs[pid].ysib is not None:
-          PCBs[parent].fchild = PCBs[pid].ysib
-          PCBs[PCBs[pid].ysib].osib = None
-        else:
-          PCBs[parent].fchild = None
-      else:
-        PCBs[PCBs[pid].osib].ysib = PCBs[pid].ysib
-        PCBs[PCBs[pid].ysib].osib = PCBs[pid].osib
-      del PCBs[pid]
-      break
-    else:
-      destroy_v2(PCBs,PCBs[pid].fchild)
+def v1_destroy(PCB,process):
+  parent = PCB[process].parent
+  PCB[parent].remove_child(process)
+  while PCB[process].children != []:
+    for child_index  in PCB[process].children:
+      v1_destroy(PCB,child_index)
+  #print(f"Destroying: {process}")
+  del PCB[process]  
 
-
-def v2(command_list):
-  print("v2:")
-  pid=1
-  v2_PCBs = {}
-  v2_PCBs[0] = PCBv2(-1)
-
-  for command in command_list:
-    if command.command == 'create':
-      PID_parent = command.n
-      print(f"Trying to Create {pid} with Parent {PID_parent}")
-      if PID_parent in v2_PCBs:
-        
-        #check for Older Sibiling:
-        if v2_PCBs[PID_parent].fchild != None:
-          osib = find_OlderSib(v2_PCBs,v2_PCBs[PID_parent].fchild)
-          v2_PCBs[pid] = PCBv2(PID_parent,osib)
-          v2_PCBs[osib].ysib = pid
-        else:
-          v2_PCBs[PID_parent].fchild = pid
-          v2_PCBs[pid] = PCBv2(PID_parent)
-        print(f"Created {pid} with Parent {PID_parent}")
-        pid+=1
-      else:
-        print(f"ERROR: Parent {command.n} does not exist")
-    elif command.command == 'destroy':
-      pid = command.n
-      print(f"Trying to destroy {pid}")
-      destroy_v2(v2_PCBs,pid)
-    showV2(v2_PCBs)
-
-
-class commands:
-  def __init__(self, command, n):
-    self.command = command
-    self.n = n
-
-
-
-def showProcessInfo(PCBs):
+def v1_showProcessInfo(PCBs):
   for x in PCBs:
     if PCBs[x].children!=[]:
       children_list = ""
@@ -124,70 +73,184 @@ def showProcessInfo(PCBs):
         children_list= children_list + c
       print(f"Process {x}: parent is {PCBs[x].parent} and children are {children_list}")
     else:
-      print(f"Process {x}: parent is {PCBs[x].parent}, and has no children")
+      print(f"Process {x}: parent is {PCBs[x].parent} and has no children")
+  print("")
 
-def destroy(process,PCBs):
-  print(f"Destroy {process}:")
-  parent_index = PCBs[process].parent
+#***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***V2***#
+def v2(commands,show_output):
+  PCB = create_V2_PCB()
+  command_num = 0
+  for command in commands:
+    command_num+=1
+    #print(f"\nCommand #{command_num}\n")
+    Action=command[0]
+    if command[1] not in PCB:
+      print(f"Error | Object Does Not exist in PCB")
+    else:
+      match Action:
+        case 'c':
+          parent = command[1]
+          #print(f"\nRunning command 'Create' with Parent {parent}")
+          process = First_Available_Process(PCB)
+          #print(f"Creating Process {process} with parent {parent}")
+          v2_create(PCB,process,parent)
+        case 'd':
+          process = command[1]
+          #print(f"\nRunning command 'Destroy' process {process}")
+          #print(f"Deleting Process {process}, and all children")
+          v2_destroy(PCB,process)
+      if show_output:
+        v2_showProcessInfo(PCB)
+  
+def v2_showProcessInfo(PCB):
+  #print("\nCurrent Processes:")
+  for process in PCB:
+    parent = PCB[process].Parent
+    children = None
+    if PCB[process].First_Child is not None:
+      First_Child = PCB[process].First_Child
+      child = First_Child
+      children = str(First_Child)
+      while True:
+        if PCB[child].Younger_Sibiling is not None:
+          Younger_Sibiling = PCB[child].Younger_Sibiling
+          children+=f" {Younger_Sibiling}"
+          child = Younger_Sibiling
+        else:
+          break
+      print(f"Process {process}: Parent is {parent} Children are {children}")
+    else:
+      print(f"Process {process}: Parent is {parent} and has no children.")
+  print("")
 
-  PCBs[parent_index].children.remove(process)
-  print(PCBs[process].children)
-  while PCBs[process].children != []:
-    for child_index  in PCBs[process].children:
-      destroy(child_index,PCBs)
-  print(f"Destroying: {PCBs[process]}")
-  del PCBs[process]  
-
-def v1(command_list):
-  print("v1:")
-  p = 1
-  v1_PCBs = {}
-  v1_PCBs[0] = PCB(-1)
-  for command in command_list:
-    if command.command == 'create':
-      print(f"Trying to Create {p} with Parent {command.n}")
-      if command.n in v1_PCBs:
-        print(f"Created {p} with Parent {command.n}")
-        v1_PCBs[p] = PCB(command.n)
-        v1_PCBs[command.n].__set_children__(p)
-        p = p + 1
-      else:
-        print("Parent DNE!")
-    elif command.command == 'destroy':
-      print(f"Destroy {command.n}:")
-      destroy(command.n,v1_PCBs)
     
-    showProcessInfo(v1_PCBs)
 
 
+def v2_destroy(PCB,process):
+  #print(f"Destroy {process}")
+  while True:
+    if PCB[process].First_Child != None: v2_destroy(PCB,PCB[process].First_Child)
+    if PCB[process].First_Child == None: break
+  Parent = PCB[process].Parent
+  if PCB[Parent].First_Child == process:
+    if PCB[process].Younger_Sibiling == None:
+      PCB[Parent].First_Child = None
+    else:
+      PCB[Parent].First_Child = PCB[process].Younger_Sibiling
+  else:
+    Older_Sibiling = PCB[process].Older_Sibiling
+    Younger_Sibiling = PCB[process].Younger_Sibiling
+    if PCB[Younger_Sibiling] == None:
+      PCB[Older_Sibiling].Younger_Sibiling = None
+    else:
+      PCB[Older_Sibiling].Younger_Sibiling = Younger_Sibiling
+      PCB[Younger_Sibiling].Older_Sibiling = Older_Sibiling
+  del PCB[process]
+  #print(f"Process {process} has been deleted")
 
 
+def find_Older_Sibiling(PCB,Parent):
+  Older_Sibiling = PCB[Parent].First_Child
+  while True:
+    if PCB[Older_Sibiling].Younger_Sibiling is not None: Older_Sibiling = PCB[Older_Sibiling].Younger_Sibiling
+    else:
+      return Older_Sibiling
+    
+def v2_create(PCB,process,Parent):
+  try:
+    #Link to Parent
+    #Check First_Child
+    if Parent not in PCB:
+      print(f"ERROR | Parent {Parent} does not exist!")
+      return
+    if PCB[Parent].First_Child is None: #No Sibilings
+      PCB[Parent].First_Child = process
+      PCB[process] = V2_Process(Parent)
+      return
+    else:
+      Older_Sibiling = find_Older_Sibiling(PCB,Parent)
+      PCB[Older_Sibiling].Younger_Sibiling = process
+      PCB[process] = V2_Process(Parent,Older_Sibiling)
+  except:
+    print(f"Error, Parent doesn't exist")
+
+#***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***MAIN***#
+#                                              #Initialize PCBs and initial Processes
+def create_V1_PCB():
+  PCBs_V1 = {}
+  PCBs_V1[0] = V1_Process(-1)
+  return PCBs_V1
+def create_V2_PCB():
+  PCBs_V2 = {}
+  PCBs_V2[0] = V2_Process(-1)
+  return PCBs_V2
+#                                                     For Both V1 and V2
+def First_Available_Process(PCB):
+  for process_id in range (1,16):
+    if process_id not in PCB:
+      return process_id
+  print("No Available Space")
+  return False
+#                                                     #Initial Functions
+def get_commands():
+  Commands = []
+  while True:
+    New_input = Request_Input()
+    Action = New_input[0]
+    process = New_input[1]
+    if Action == 'e': break
+    if Action == False: 
+      print(f"Error: {process}")
+    else:
+      Commands.append(New_input)
+  return Commands
+def Request_Input():
+  try:
+    new_input = input("Enter commands of the form 'create N', 'destroy N', or 'end', where N is an integer between 0 and 15:")
+    if new_input == "end": return ['e',None]
+    new_input = new_input.split(" ")
+    action = new_input[0]
+    item = int(new_input[1])
+    if action != "create" and action != "destroy" and action != "end": return [False, "Invalid action item"]
+    if item not in range(0,16): return [False, "Invalid Process"]
+    match action:
+      case "create":
+        return ['c',item]
+      case "destroy":
+        if item == 0: return [False,"Invalid Argument"]
+        return ['d',item]
+        
+  except:
+    print("Invalid Argument")
+    return [False,"Invalid Argument"]
 
 def main():
-  commands_list = list()
-  while True:
-    c = input("enter commands of the form 'create N', 'destroy N', or 'end', where N is an integer between 0 and 15:")
-    c = c.split(" ")
-    if c[0] == 'end':
-      break
-    if len(c) == 2:
-      if c[0] == 'create' or c[0] == 'destroy':
-        if int(c[1]) in range(0,16):
-          commands_list.append(commands(c[0],int(c[1])))
-        else:
-          ic()
-      else:
-        ic()
-    else:
-      ic()
-  v1(commands_list)
-  v2(commands_list)
-  
-
-
-
-def ic():
-  print("Invalid command")
+  Commands=get_commands()
+  print("Beginning V1:\n`")
+  v1(Commands,True)
+  print("\nEnd of V1\n")
+  print("Beginning of V2:\n")
+  v2(Commands,True)
+  print("\nEnd of V2")
+  input("Press Enter to start timings")
+  print("Running V1 200 Times")
+  v1_start = time.process_time_ns()
+  for _ in range(200):
+    v1(Commands,False)
+  v1_end = time.process_time_ns()
+  print("Running V1 200 Times")
+  v2_start = time.process_time_ns()
+  for _ in range(200):
+    v1(Commands,False)
+  v2_end = time.process_time_ns()
+  v1_running = v1_end-v1_start
+  v2_running = v2_end-v1_start
+  print(f"V1 start time: {v1_start}")
+  print(f"V1 end time: {v1_end}")
+  print(f"V1 Running Time: {v1_running}")
+  print(f"V2 start time: {v2_start}")
+  print(f"V2 end time: {v2_end}")
+  print(f"V2 Running Time: {v2_running}")  
 
 main()
 
